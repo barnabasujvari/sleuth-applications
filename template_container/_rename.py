e14c61804f240008e1a3c20eaa015683bd068b1e
Christoph Minixhofer <minchc11@htl-kaindorf.ac.at>
2020-11-17 17:44:04 +0000

    ignore git files

--- template_container/_rename.py
+++ template_container/_rename.py
@@ -16,18 +16,19 @@ def replace_in_file(filename, search, replace):
 def _rename(search, replace):
     for dirpath, dirnames, filenames in os.walk("."):
         for filename in filenames:
-            full_path = os.path.join(dirpath, filename)
-            new_path = full_path.replace(search, replace)[1:]
-            try:
-                Path(dirpath.replace(search, replace)).mkdir(parents=True, exist_ok=True)
-            except FileExistsError:
-                pass
-            os.rename(os.getcwd() + full_path[1:], os.getcwd() + new_path)
-            full_path = os.getcwd() + new_path
-            try:
-                replace_in_file(full_path, search, replace)
-            except UnicodeDecodeError:
-                pass
+            if '.git' not in dirpath:
+                full_path = os.path.join(dirpath, filename)
+                new_path = full_path.replace(search, replace)[1:]
+                try:
+                    Path(dirpath.replace(search, replace)).mkdir(parents=True, exist_ok=True)
+                except FileExistsError:
+                    pass
+                os.rename(os.getcwd() + full_path[1:], os.getcwd() + new_path)
+                full_path = os.getcwd() + new_path
+                try:
+                    replace_in_file(full_path, search, replace)
+                except UnicodeDecodeError:
+                    pass
 
 if __name__ == "__main__":
     _rename()
