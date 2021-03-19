4dca878d5cbff49a0f1fe222e0ad0d93bb82496f
Patrick Tourniaire <patrick@tourniaire.net>
2021-03-19 19:06:01 +0000

    Added comments file

--- api/get_ticker.py
+++ api/get_ticker.py
@@ -6,22 +6,13 @@ from pathlib import Path
 Given a comment string that has been retrieved from a scraping source we
 need to find the mentioned stock tickers in that comment. For this implementation
 you are assuming that the comments are scraped for you and you can run
-poetry run pytest to check your impementation.
+poetry run pytest to check your impementation. This only has a few examples
+so if you wish to write more tests you can have a look in the tests/comments.txt file.
 
 You should also check if the stock ticker is a valid one. Which means that you
 need to check if this is an actual ticker listed on the stock exchange.
 
-The return is a list of string of stock tickers.
-
-Examples (retrieved for r/wallstreetbets):
-In: YOLOing my stimmy tomorrow during the short attack. Valhalla is waiting for us
-Out: []
-
-In: This is the 8th market day in a row that GME has closed above $200 GME has not closed below $100 since February 25th, 22 days in a row
-Out: ["GME"]
-
-In: PSYCH!!!!! AH is gonna be a snore fest but come PM we gonna see the rocket boosters engage and we are going straight to the fucking moon
-Out: []
+The return is a list of strings of stock tickers.
 """
 def get(comment):
     print(comment)
