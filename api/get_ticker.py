ff4fb7b380b467b4ed6497769acd48f4770dbd3e
Barnabas Ujvari <s1758100@ed.ac.uk>
2021-03-26 11:51:46 +0100

    assignment done

--- api/get_ticker.py
+++ api/get_ticker.py
@@ -1,6 +1,7 @@
 import os
 import json
 from pathlib import Path
+import regex as re
 
 """
 Given a comment string that has been retrieved from a scraping source we
@@ -14,7 +15,35 @@ need to check if this is an actual ticker listed on the stock exchange.
 
 The return is a list of strings of stock tickers.
 """
-def get(comment):
-    print(comment)
 
-    return "Not implemented", 501
+def read_3_char_NASDAQ_tickers():
+    '''
+    for memory efficiency reasons I only include 3 char or longer names, as I only search for those anyway
+    '''
+    ticker_dict = {}
+    # csv dowloaded from https://www.nasdaq.com/market-activity/stocks/screener without any filter
+    with open("nasdaq_screener.csv") as f:
+        for line in f.readlines():
+            ticker = line.strip().split(',')[0]
+            if len(ticker)>2:
+                ticker_dict[ticker] = 1
+    return ticker_dict
+
+# reading once for efficiency
+ticker_dict = read_3_char_NASDAQ_tickers()
+
+def get(comment):
+    global ticker_dict
+    # argument for using only the 3 char or longer tickers, is that 1 or 2 uppercase letters would have such high rate of false positives (DD and A are valid stock tickers but are likely not mentioned as a stock in most cases) that I coinsidered them better to
+    # not include
+    potential_tickers = re.findall(r'[A-Z]{3,}',comment)
+    tickers = []
+    blacklist = ["WSB", "YOLO", "ETH", "BTC"]
+    for tic in potential_tickers:
+        if (tic not in blacklist) and (tic in ticker_dict) and (tic not in tickers):
+            tickers.append(tic)
+
+    return tickers, 200
+
+
+
